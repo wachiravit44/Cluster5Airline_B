8f087b24b30db274c8d37d110fd56d1c3486a0d5
wachiravit44 <garfieldandfield@gmail.com>
2020-10-16 23:27:39 +0700

    Create All_include_in_main.h

---
+++ All_include_in_main.h
@@ -0,0 +1,10 @@
+#include <iostream> 
+#include <fstream> 
+#include <string>
+#include <iomanip> 
+#include <cstdlib>
+#include <sstream>
+#include <cstddef>  
+#include <ctime>
+#include "Function.h"
+using namespace std;
